127ad39076d00c9d84f3168365f9bdcabe294eaf
maggieliz <maggielizward@gmail.com>
2021-07-15 19:26:57 +0100

    Basic HTML and CSS are done. Set up the mini-grid in js and css.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-console.log('hello')
+// Ultimate Tic Tac TouchEvent
+// A play button should start the game 
+
+// A two player game. Players alternate placing their marker in a cell in the table. The objective is to get three markers in a row in each small game. If you've gotten three mini-games in a row, the game is won. If no player can get three in a row and the cells are all filled, it's a draw.
+
+// DOM Variables
+
+const miniGrid = document.querySelector('.mini-grid')
+const newGameBtn = document.querySelector('.new-game')
+
+// Game Variables
+
+const width = 3
+const miniGridCellCount = width * width
+const cells = []
+
+// Functions
+
+function createMiniGrid() {
+  for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div')
+    cell.setAttribute('data-index', i)
+    cells.push(cell)
+    miniGrid.appendChild(cell)
+  }
+}
+createMiniGrid()
